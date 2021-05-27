590c23a3c6d184ff37dd6812ea307728fe84a3b7
kockceu <danieltuba1@gmail.com>
2021-05-27 20:31:15 -0300

    initial commit

--- Projeto_teste.py
+++ Projeto_teste.py
@@ -1,8 +1,9 @@
 
-import urllib.request
+# Bibliotecas
 import json
-import requests
+import requests #para solicitações http
 
+#Função 
 def CEPConsulta():
     print('+++++++++++++++++++++')
     print('######Consultando o  CEP ######')
@@ -10,11 +11,12 @@ def CEPConsulta():
     print()
 
     cep_input = input('Digite o CEP para a consulta: ')
-
+#Inserindo uma opção caso passe quantidades do número do cep
     if len(cep_input) != 8:
         print('Quantidade de digitos inválido')
         exit()
 
+#API com alteração no cep e colocando um dicionário
     request = requests.get('https://viacep.com.br/ws/{}/json/'.format(cep_input))
 
     addres_data = request.json()
@@ -26,9 +28,12 @@ def CEPConsulta():
         print('Bairro: {}'.format(addres_data['bairro']))
         print('Cidade: {}'.format(addres_data['localidade']))
         print('Estado: {}'.format(addres_data['uf']))
-        
+
+# caso de erro de Cep:
     else:
         print('{}: CEP inválido.'.format(cep_input))
+
+#opção para colocar dar continuidade
 
     opcao = int(input('Deseja realizar uma nova consulta ?\n1. Sim \n2. Sair\n'))
     if opcao == 1:
